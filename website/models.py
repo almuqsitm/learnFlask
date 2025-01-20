82ff44701144671ca545d0518df61cecc98e3dfa
Mohammed Al-Muqsit <almuqsit101@gmail.com>
2025-01-19 17:44:41 -0800

    Fixed issue with time --> now shows time in EST in  %Y-%d-%d %H:%M:%S

--- website/models.py
+++ website/models.py
@@ -1,12 +1,24 @@
+import pytz
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
+from datetime import datetime
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    date = db.Column(db.DateTime(timezone=True))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, data, user_id, date=None):
+        self.data = data
+        self.user_id = user_id
+        if date is None:
+            est_tz = pytz.timezone('America/New_York')
+            est_now = datetime.now(est_tz).replace(microsecond=0)
+            self.date = est_now
+        else:
+            self.date = date.replace(microsecond=0)
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
--- website/views.py
+++ website/views.py
@@ -3,6 +3,8 @@ from flask_login import login_required, current_user
 from .models import Note
 from . import db
 import json
+import pytz
+from datetime import datetime
 
 views = Blueprint('views', __name__)
 
@@ -19,10 +21,19 @@ def home():
             db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
-    return render_template("home.html", user=current_user)
+
+    # Convert note dates to Eastern Standard Time (EST)
+    est_tz = pytz.timezone('America/New_York')
+    notes = []
+    for note in current_user.notes:
+        est_date = note.date.astimezone(est_tz)
+        formatted_date = est_date.strftime('%Y-%m-%d %H:%M:%S')
+        notes.append({'data': note.data, 'date': formatted_date, 'id': note.id})
+
+    return render_template("home.html", user=current_user, notes=notes)
 
 @views.route('/delete-note', methods=['POST'])
-def delete_node():
+def delete_note():
     note = json.loads(request.data)
     noteId = note['noteId']
     note = Note.query.get(noteId)
@@ -32,5 +43,3 @@ def delete_node():
             db.session.commit()
     
     return jsonify({})
-
-    
